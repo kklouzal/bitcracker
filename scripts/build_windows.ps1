8b3d9a04019cf4a674338f001522719f89dc464e
Kyle Klouzal <kklouzal@users.noreply.github.com>
2026-06-03 01:04:29 +0000

    Pass Windows build flags explicitly

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -10,7 +10,6 @@ function Invoke-Checked {
     param(
         [Parameter(Mandatory = $true)]
         [string] $File,
-        [Parameter(ValueFromRemainingArguments = $true)]
         [string[]] $Arguments
     )
 
@@ -20,25 +19,30 @@ function Invoke-Checked {
     }
 }
 
-Invoke-Checked cl.exe `
-    /nologo /O2 /W3 /bigobj /I $Compat /D_CRT_SECURE_NO_WARNINGS `
-    "/Fe:$Build\bitcracker_hash.exe" `
+Invoke-Checked -File cl.exe -Arguments @(
+    "/nologo", "/O2", "/W3", "/bigobj", "/I", $Compat,
+    "/D_CRT_SECURE_NO_WARNINGS",
+    "/Fe:$Build\bitcracker_hash.exe",
     "$Root\src_HashExtractor\bitcracker_hash.c"
+)
 
-Invoke-Checked cl.exe `
-    /nologo /O2 /W3 /bigobj /I $Compat /D_CRT_SECURE_NO_WARNINGS `
-    "/Fe:$Build\bitcracker_rpgen.exe" `
+Invoke-Checked -File cl.exe -Arguments @(
+    "/nologo", "/O2", "/W3", "/bigobj", "/I", $Compat,
+    "/D_CRT_SECURE_NO_WARNINGS",
+    "/Fe:$Build\bitcracker_rpgen.exe",
     "$Root\src_RPGenerator\bitcracker_rpgen.c"
+)
 
-Invoke-Checked nvcc.exe `
-    -I $Compat `
-    -gencode "arch=compute_89,code=sm_89" `
-    -Xcompiler "/O2" `
-    -Xcompiler "/D_CRT_SECURE_NO_WARNINGS" `
-    -o "$Build\bitcracker_cuda.exe" `
-    "$Root\src_CUDA\main.cu" `
-    "$Root\src_CUDA\cuda_attack.cu" `
-    "$Root\src_CUDA\utils.cu" `
+Invoke-Checked -File nvcc.exe -Arguments @(
+    "-I", $Compat,
+    "-gencode", "arch=compute_89,code=sm_89",
+    "-Xcompiler", "/O2",
+    "-Xcompiler", "/D_CRT_SECURE_NO_WARNINGS",
+    "-o", "$Build\bitcracker_cuda.exe",
+    "$Root\src_CUDA\main.cu",
+    "$Root\src_CUDA\cuda_attack.cu",
+    "$Root\src_CUDA\utils.cu",
     "$Root\src_CUDA\w_blocks.cu"
+)
 
 Get-ChildItem $Build
